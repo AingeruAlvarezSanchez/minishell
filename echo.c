6c8a572c8273b2023c108a6700f6718f2cc6c22f
AingeruAlvarez <aingeru.alvarez@outlook.com>
2022-02-25 18:30:30 +0100

    echo reuploaded, now works well with multistrings

--- echo.c
+++ echo.c
@@ -1,17 +1,21 @@
 #include "minishell.h"
 
-void	ft_echo(char *str, char *flag)
+void	ft_echo(t_cmds *Cmds, char *flag)
 {
 	int	i;
+	int	j;
 
-	if (!str)
+	i = 0;
+	if (flag)
+		i = 1;
+	while (Cmds->p_command[++i])
 	{
-		write(1, "\n", 1);
-		exit(0);
+		j = -1;
+		while (Cmds->p_command[i][++j])
+			write(1, &Cmds->p_command[i][j], 1);
+		if (Cmds->p_command[i + 1])
+			write(1, " ", 1);
 	}
-	i = -1;
-	while(str[++i])
-		write(1, &str[i], 1);
 	if (flag)
 		exit(0);
 	write(1, "\n", 1);
@@ -23,11 +27,14 @@ void	ft_check_echo(t_cmds *Cmds)
 	if (!Cmds->p_command[1])
 	{
 		write(1, "\n", 1);
-		exit(0);
+		exit (0);
 	}
-	else if (!ft_strncmp(Cmds->p_command[1], "-n", 2))
-		ft_echo(Cmds->p_command[2], Cmds->p_command[1]);
 	else
-		ft_echo(Cmds->p_command[1], NULL);
+	{
+		if (!ft_strncmp(Cmds->p_command[1], "-n", 2))
+			ft_echo(Cmds, Cmds->p_command[1]);
+		else
+			ft_echo(Cmds, NULL);
+	}
 }
 
