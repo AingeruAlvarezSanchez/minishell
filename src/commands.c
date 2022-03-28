1b2a841ca72a2920b2e529be675fda0837cb1a6b
Aingeru <aingeru.alvarez@outlook.com>
2022-03-28 10:58:48 +0200

    starting newsplit

--- src/commands.c
+++ src/commands.c
@@ -6,7 +6,7 @@
 /*   By: aalvarez <aalvarez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/03 18:36:57 by ecorreia          #+#    #+#             */
-/*   Updated: 2022/03/10 16:10:45 by aalvarez         ###   ########.fr       */
+/*   Updated: 2022/03/28 10:45:54 by aalvarez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,11 +52,50 @@ void	ft_fill_commands(char *str, t_cmds *Cmds)
 	}
 }
 
+void	ft_split_dollar(t_cmds *Cmds, t_data *Data)
+{
+}
+
+void	ft_fill_cmds(char *str, t_cmds *Cmds, __attribute__((unused)) t_data *Data)
+{
+	int	i;
+	int	pipes;
+	int	dollar;
+	int	quotes;
+
+	i = -1;
+	pipes = 0;
+	dollar = 0;
+	quotes = 0;
+	while (str[++i])
+	{
+		if (str[i] == '|')
+			pipes = 1;
+		else if (str[i] == '$')
+			dollar = 1;
+		/*else if (str[i] == '"' || str[i] == '\'')
+			quotes = 1;*/
+	}
+	if (pipes == 1)
+	{
+		Cmds->commands = ft_split(str, '|');
+		i = -1;
+		while (Cmds->commands[++i])
+			Cmds->commands[i] = ft_strtrim(Cmds->commands[i], " ");
+	}
+	else if (dollar == 1)
+		ft_split_dollar(Cmds, Data);
+	/*else if (quotes == 1)
+		ft_split_quotes();*/
+}
+
 void	ft_commands(char *str, t_cmds *Cmds, t_data *Data)
 {
 	if (!str)
 		return ;
 	ft_commands_n(str, Cmds);
-	ft_fill_commands(str, Cmds);
+	ft_fill_cmds(str, Cmds, Data);
+	exit(0);
+	//ft_fill_commands(str, Cmds);
 	ft_init_exec(Cmds, Data);
 }
