9bf767b1240ffb782404bec340d48416809659b6
AingeruAlvarezSanchez <aingeru.alvarez@outlook.com>
2022-05-17 04:13:50 -0700

    now simple commands get spplitted in between pipes

--- src/pipes.c
+++ src/pipes.c
@@ -6,23 +6,52 @@
 /*   By: aalvarez <aalvarez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/16 18:08:50 by aalvarez          #+#    #+#             */
-/*   Updated: 2022/05/16 18:09:24 by aalvarez         ###   ########.fr       */
+/*   Updated: 2022/05/17 04:09:09by aalvarez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
+static void ft_pipes_multicommand(t_cmds *cmds, char **split)
+{
+    char    **tmp;
+    int     i;
+    int     x;
+
+    if (ft_doublestrlen(split) == 1)
+    {
+        ft_doublefree(split);
+        return ;
+    }
+    tmp = ft_doublestrdup(cmds->tokens);
+    cmds->tokens = (char **)malloc(sizeof(char *) * (ft_doublestrlen(tmp) + ft_doublestrlen(split)));
+    i = -1;
+    while (++i < (ft_doublestrlen(tmp) - 2))
+        cmds->tokens[i] = ft_strdup(tmp[i]);
+    x = 0;
+    while (split[x])
+        cmds->tokens[i++] = ft_strdup(split[x++]);
+    cmds->tokens[i] = ft_strdup("|");
+    cmds->tokens[i + 1] = 0;
+    ft_doublefree(split);
+    ft_doublefree(tmp);
+}
+
 static void ft_pipes_prev(t_cmds *cmds, char **tmp, int result)
 {
-    int i;
+    int     i;
+    char    *tmp2;
 
     i = -1;
     cmds->tokens = (char **)malloc(sizeof(char *) * (ft_doublestrlen(tmp) * (ft_doublestrlen(tmp) + 3)));
     while (tmp[++i])
         cmds->tokens[i] = ft_strdup(tmp[i]);
-    cmds->tokens[i] = ft_substr(cmds->prompt, 0, (result - 1));
+    tmp2 = ft_substr(cmds->prompt, 0, (result - 1));
+    cmds->tokens[i] = ft_strtrim(tmp2, " ");
     cmds->tokens[i + 1] = ft_strdup("|");
     cmds->tokens[i + 2] = 0;
+    ft_pipes_multicommand(cmds, ft_split(cmds->tokens[i], ' '));
+    free(tmp2);
 }
 
 int ft_pipes(t_cmds *cmds, int xref)
